bb9982f79a445bbdb6aba20211e6d468c7f9c3d4
psamim <p.samim@gmail.com>
2013-07-27 11:01:15 +0430

    Passive mode in FTP

--- datis.php
+++ datis.php
@@ -227,7 +227,8 @@ Options:";
         
         // Login with username and password
         $login_result = ftp_login($conn_id, $ftp_username, $ftp_password);
-        
+        ftp_pasv($conn_id, true);
+
         // Check connection
         if ((!$conn_id) || (!$login_result)) {
             echo FAIL . ": FTP connection has failed! \n";
@@ -336,6 +337,7 @@ Options:
         
         // Login with username and password
         $login_result = ftp_login($conn_id, $info['ftp']['username'], $info['ftp']['password']);
+        ftp_pasv($conn_id, true);
         
         // Check connection
         if ((!$conn_id) || (!$login_result)) {
@@ -738,7 +740,8 @@ Options:
             
             // Login with username and password
             $login_result = ftp_login($conn_id, $info['ftp']['username'], $info['ftp']['password']);
-            
+            ftp_pasv($conn_id, true);
+
             // Check connection
             if ((!$conn_id) || (!$login_result)) {
                 echo FAIL . ": FTP connection has failed! \n";
@@ -889,6 +892,7 @@ Options:
                 $conn_id      = ftp_connect($info['ftp']['server']);
                 // Login with username and password
                 $login_result = ftp_login($conn_id, $info['ftp']['username'], $info['ftp']['password']);
+                ftp_pasv($conn_id, true);
                 // Check connection
                 if ((!$conn_id) || (!$login_result)) {
                     echo FAIL . ": FTP connection has failed! \n";
@@ -1098,7 +1102,8 @@ Options:";
         
         // Login with username and password
         $login_result = ftp_login($conn_id, $info['ftp']['username'], $info['ftp']['password']);
-        
+        ftp_pasv($conn_id, true);
+
         // Check connection
         if ((!$conn_id) || (!$login_result)) {
             echo FAIL . ": FTP connection has failed! \n";
