b6b27d4af8126116a1cb104676e88afadbb8fafc
Samim Pezeshki <p.samim@gmail.com>
2013-10-27 15:52:48 +0330

    Database class cleanings

--- class/db.class.php
+++ class/db.class.php
@@ -14,56 +14,80 @@ class Db
 
 	public static function backup($is_local)
 	{
-        global $pwd, $file, $info, $table, $structure;
+        global $table, $structure;
         $table = (isset($table)) ? $table : 'all';
+        $structure = (isset($structure)) ? 1 : 0;
+
+		// Warning for server restore
         if ($table == 'all' && isset($structure)) {
-            echo WARNING . ": DATA WILL BE LOST UPON RESTORE!! \n         Are you sure to get ONLY schema of all tables? (enter fuckme to continue)\n";
-            if (str_replace("\n", '', fgets(STDIN)) != 'fuckme') {
+            echo WARNING . ": DATA WILL BE LOST UPON RESTORE!! \n         Are you sure to get ONLY schema of all tables? (yes/no))\n";
+            if (str_replace("\n", '', fgets(STDIN)) != 'yes') {
                 bye();
             }
         }
 
-        $structure = (isset($structure)) ? 1 : 0;
-
         if ($is_local) {
-            exec("php '{$pwd}/dump.php' backup {$table} {$structure} " . ((isset($file)) ? $file : ''), $return, $exit);
-			$return = '';
-            echo (($exit == 0) ? SUCCESS : FAIL) . ": " . self::$error[$exit] . PHP_EOL;
+			self::local_backup();
         } else {
-            $result = file_get_contents("http://" . $info['ftp']['server'] . "/dump.php?a1=backup&a2={$table}&a3={$structure}&m=" . rand(1, 1000));
-            echo (($result == 0) ? SUCCESS : FAIL) . ": " . self::$error[$result] . "\n";
-            if ($result == 0) {
-                exec("wget -O '$file' {$info['ftp']['server']}/sql.gz?rand=" . rand(1, 1000), $return, $exit);
-                if ($exit == 0) {
-                    echo SUCCESS . ": Backup successfuly saved to '$file'\n";
-                } else {
-                    echo FAIL . ": Failed, something happened during download.\n";
-                }
+			self::server_backup();
+        }
+	}
+
+	public static function local_backup()
+	{
+        global $pwd, $file, $table, $structure;
+        exec("php '{$pwd}/dump.php' backup {$table} {$structure} " . ((isset($file)) ? $file : ''), $return, $exit);
+		$return = '';
+        echo (($exit == 0) ? SUCCESS : FAIL) . ": " . self::$error[$exit] . PHP_EOL;
+	}
+
+	public static function server_backup()
+	{
+        global $file, $info, $table, $structure;
+        $result = file_get_contents("http://" . $info['ftp']['server'] . "/dump.php?a1=backup&a2={$table}&a3={$structure}&m=" . rand(1, 1000));
+        echo (($result == 0) ? SUCCESS : FAIL) . ": " . self::$error[$result] . "\n";
+        if ($result == 0) {
+            exec("wget -O '$file' {$info['ftp']['server']}/sql.gz?rand=" . rand(1, 1000), $return, $exit);
+			$return = 0;
+            if ($exit == 0) {
+                echo SUCCESS . ": Backup successfuly saved to '$file'\n";
+            } else {
+                echo FAIL . ": Failed, something happened during download.\n";
             }
         }
 	}
 
     public static function restore ($is_local)
     {
-		global $ftp;
         // TODO: Temporary
         // if ($is_local === false) {
         // echo NOTICE . ": Restore to server is not supported yet :) \n";
         // return
         // }
-        global $pwd, $file, $info;
         if ($is_local) {
-            copy(dirname(__FILE__) . "/../inc/dump.php", $pwd . '/dump.php');
-            exec("php '{$pwd}/dump.php' restore " . $file, $return, $exit) . "";
-			$return = '';
-            echo (($exit == 0) ? SUCCESS : FAIL) . ": " . self::$error[$exit] . PHP_EOL;
+			self::local_restore();
         } else {
-            if (!$ftp->put($file, '/sql.gz', false)) {
-				return false;
-			}
-            $result = file_get_contents("http://" . $info['ftp']['server'] . "/dump.php?a1=restore&" . rand(1, 1000));
-            echo (($result == 0) ? SUCCESS : FAIL) . ": " . self::$error[$result] . PHP_EOL;
+			self::server_restore();
         }
     }
+
+	public static function local_restore()
+	{
+        global $pwd, $file;
+        copy(dirname(__FILE__) . "/../inc/dump.php", $pwd . '/dump.php');
+        exec("php '{$pwd}/dump.php' restore " . $file, $return, $exit) . "";
+		$return = '';
+        echo (($exit == 0) ? SUCCESS : FAIL) . ": " . self::$error[$exit] . PHP_EOL;
+	}
+
+	public static function server_restore()
+	{
+        global $file, $info, $ftp;
+        if (!$ftp->put($file, '/sql.gz', false)) {
+			return false;
+		}
+        $result = file_get_contents("http://" . $info['ftp']['server'] . "/dump.php?a1=restore&" . rand(1, 1000));
+        echo (($result == 0) ? SUCCESS : FAIL) . ": " . self::$error[$result] . PHP_EOL;
+	}
 
 }
