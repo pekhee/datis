d7c398e74e3aae862aeacb0bfd0904a3719074cb
Samim Pezeshki <p.samim@gmail.com>
2013-10-29 13:02:42 +0330

    Download added

--- mods/download.php
+++ mods/download.php
@@ -17,6 +17,9 @@ Options:
 
 -d <DIR>              Download  all files in givern directory.
 --directory=<DIR>
+
+-b                    Do not unzip, only backup
+--backup
 ";
 
 /**
@@ -38,13 +41,16 @@ foreach ($args as $key => $value) {
     case 'directory':
         $zip_directory = $value;
         break;
+	case 'b':
+	case 'bakup':
+		$backup = true;
+		break;
     }
 }
 
 /**
  * START
  */
-
 $ftp = new Ftp($info);
 if (!$ftp->connect()) {
 	$result == false;
@@ -52,8 +58,8 @@ if (!$ftp->connect()) {
 }
 
 $error = array(
+    'Cannot zip!',
     'Zip was done succcessfully.',
-    'Cannot zip!'
 );
 
 // Upload dump
@@ -68,11 +74,11 @@ if (!$ftp->put($config['temp'] . '/zend/main/dump.php', '/dump.php')) {
 
 // Zip
 $zip_directory = urlencode(((isset($zip_directory)) ? $zip_directory : './userfiles/'));
-$file = (isset($file)) ? $file : 'zip.zip';
+$file = (isset($file_override)) ? $file_override : 'zip.zip';
 $get_url = "http://" . $info['ftp']['server'] . "/dump.php?a1=zip&a2={$zip_directory}&rand=" . rand(1, 1000);
 $zip = file_get_contents($get_url);
 var_dump($zip);
-echo (($zip == 0) ? SUCCESS : FAIL) . ": " . $error[$zip] . PHP_EOL;
+echo (($zip =! 0) ? SUCCESS : FAIL) . ": " . $error[$zip] . PHP_EOL;
 
 // Download
 exec("wget -O '$file' {$info['ftp']['server']}/zip.zip?rand=" . rand(1, 1000), $return, $exit);
@@ -87,3 +93,20 @@ if ($exit == 0) {
 $ftp->del('dump.php');
 $ftp->del('zip.zip');
 $ftp->close();
+
+
+// Unzip
+if (!isset($backup)) {
+	$error = array(
+    	'Unzip was done succcessfully.',
+    	'Cannot unzip!'
+	);
+	copy(dirname(__FILE__) . "/../inc/dump.php", $pwd . '/dump.php');
+	exec("php '{$pwd}/dump.php' unzip {$file} ", $return, $exit);
+	$return = '';
+	echo (($exit == 0) ? SUCCESS : FAIL) . ": " . $error[$exit] . PHP_EOL;
+	unlink($pwd . '/dump.php');
+	unlink($file);
+}
+
+unlink($pwd . '/dump.php');
